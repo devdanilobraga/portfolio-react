115c0448f4a3811a8f63b65afa84878a24987064
Danilo Braga <danilobragam@gmail.com>
2025-05-28 06:50:56 -0300

    commit

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -1,8 +1,31 @@
+import './Projetos.scss';
+import projetos from './projetos.json';
+import { FaExternalLinkAlt } from 'react-icons/fa';
+
 function Projetos() {
   return (
-    <div  id="projetos">
-      <h1> Tudo Projetos!</h1>
-    </div>
+    <section className="projetos" id="projetos">
+      <h2>Meus Projetos</h2>
+      <div className="projetos-grid">
+        {projetos.map((proj, index) => (
+          <div className="projeto-card" key={index}>
+            <img src={proj.image} alt={proj.title} />
+            <h3>{proj.title}</h3>
+            <p>{proj.description}</p>
+            <div className="tecnologias">
+              {proj.technologies.map((tec, i) => (
+                <span key={i}>{tec}</span>
+              ))}
+            </div>
+            <div className="links">
+              <a href={proj.link} target="_blank" rel="noreferrer">
+                <FaExternalLinkAlt /> Ver Projeto
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
 
